8ba95af1e2f5035c5d26425c32049c7d1ca647fe
Kauã <98705115+KauaZs@users.noreply.github.com>
2022-10-17 14:59:18 +0000

    up

--- commands/util/setup.js
+++ commands/util/setup.js
@@ -6,6 +6,8 @@ module.exports = {
   run: async(client, message, args) => {
     const channelData = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
     if(!channelData) return message.reply(`:x: Canal não encontrado!`)
+    if(channelData.type !== 0) return message.reply(`:x: O canal informado não é de texto!`)
+    if(channelData.nsfw) return message.reply(`:x: O canal informado está com nsfw ativado!`)
     let channels = message.guild.channels.cache.map(x => x.id)
     if(channelData.id.includes(channels)) return message.reply(`:x: Este canal já está definido!`)
     db.push(`channels`, channelData.id)
--- events/client/messageCreate.js
+++ events/client/messageCreate.js
@@ -3,18 +3,14 @@ const db = require('secure-db')
 
  client.on('messageCreate', async (message) => {
   if(message.author.bot) return;
-  const prefix = 'g?';
+  const prefix = 'g?';    if(message.content.replace('!','').startsWith(`<@${client.user.id}`)) return message.reply('oi')
 
-  if(message.content.replace('!','').startsWith(`<@${client.user.id}`)) return message.reply('oi')
-
-  let channelsData = db.get(`channels`)
-  if(channelsData?.includes(message.channel.id)) {
+  let channelsData = db.get(`channels`) 
+ if(channelsData?.includes(message.channel.id)) {
 require('../../client/global.js').run(client, message)
   }
-  if(!message.content.startsWith(prefix)) return;
-  
+  if(!message.content.startsWith(prefix)) return;  
   const args = message.content.slice(prefix.length).trim().split(' ');
-
   const command = args.shift().toLowerCase()
  let cmd = client.commands.get(command) || client.commands.find(als => als.aliases && als.aliases.includes(command));
 
